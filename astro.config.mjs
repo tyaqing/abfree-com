da8cbbd18c5aab99db6ce9e122f3d8db0cc19767
yakirteng <yakirteng@tencent.com>
2024-05-16 01:16:00 +0800

    vercel换成static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import simpleStackForm from "simple-stack-form";
 import {defineConfig} from "astro/config";
 import astroI18next from "astro-i18next";
